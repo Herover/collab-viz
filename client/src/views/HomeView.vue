8866028259a93c7e6c357c5f14495f5a0d51dfa7
Herover <hi+git@leonora.app>
2022-08-09 17:41:37 +0200

    Remove default vue elments, colors

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import VizEditor from "@/components/VizEditor.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <VizEditor />
   </main>
 </template>
