ec01a258500aaca3cd0543f6b435c6bcba5aa2bd
Herover <hi+git@leonora.app>
2022-08-11 22:26:42 +0200

    Add scale type dropdown

--- client/src/components/ScaleEditor.vue
+++ client/src/components/ScaleEditor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import DropdownInput from "./DropdownInput.vue";
 
 const emit = defineEmits<{
   (event: "op", data: unknown): void;
@@ -18,13 +19,46 @@ watch(
   }
 );
 
-const changed = () => {
-  emit("op", ["name", { r: 0, i: name.value }]);
+const type = ref(props.scale.type);
+watch(
+  () => props.scale.type,
+  () => {
+    type.value = props.scale.type;
+  }
+);
+
+const quantitativeScales = [
+  "linear",
+  "log",
+  "pow",
+  "sqrt",
+  "symlog",
+  "time",
+  "utc",
+  // "sequential",
+];
+const discreteScales = ["ordinal", "band", "point"];
+const discretizingScales = ["quantile", "quantize", "threshold", "bin-ordinal"];
+const allScales = quantitativeScales
+  .concat(discreteScales)
+  .concat(discretizingScales);
+
+const changed = (k: string, op: any) => {
+  emit("op", [k, op]);
 };
 </script>
 
 <template>
-  <div>
-    <h3><input v-model="name" @input="changed" /></h3>
+  <div class="pane-holder">
+    <input
+      v-model="name"
+      @input="() => changed('name', { r: 0, i: name.value })"
+    />
+    <DropdownInput
+      :value="type"
+      :options="allScales"
+      label="Type"
+      @op="(op) => changed('type', op)"
+    />
   </div>
 </template>
