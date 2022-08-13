6706bda29d053364bd62711720aab8128d645742
Herover <hi+git@leonora.app>
2022-08-13 23:18:51 +0200

    Add number input component

---
+++ client/src/components/NumberInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const emit = defineEmits<{
+  (event: "op", data: unknown): void;
+}>();
+
+const props = defineProps<{
+  value: number;
+  label: string;
+}>();
+
+const value = ref(props.value);
+watch(
+  () => props.value,
+  () => {
+    value.value = props.value;
+  }
+);
+
+const changed = () => {
+  emit("op", [{ r: 0, i: value.value }]);
+};
+
+// TODO: Find better way to get unique identifier
+const id = Math.random() + "";
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{ props.label }}</label>
+    <input :id="id" type="number" v-model="value" @change="changed" />
+  </div>
+</template>
